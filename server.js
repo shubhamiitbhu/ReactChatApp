9b510e67e3c4f91745c672d5d26c79e4afa1ac6f
shubhamiitbhu <shubhamk.ojha.civ16@iitbhu.ac.in>
2019-03-06 01:13:08 +0530

    to heroku

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var socket = require('socket.io');
-
+var path=require('path');
 var app = express();
 
+if(process.env.NODE_ENV === 'production')
+{
+	app.use(express.static('client/build'));
+	app.get('*', (req,res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
-server = app.listen(5000, function(){
+
+server = app.listen(process.env.PORT || 5000, function(){
     console.log('server is running on port 5000')
 });
 
