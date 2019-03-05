2c26ae0104c4b21c81c331ced9e60c2382b32a6f
dwiveditanuj41 <tanuj.dwivedi.civ16@itbhu.ac.in>
2019-03-06 04:50:43 +0530

    heroku update

---
+++ Client/build/precache-manifest.4eef093da5022e0447f8f888de61c2d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bfe609acb6eecbcb50ce",
+    "url": "/static/js/main.bfe609ac.chunk.js"
+  },
+  {
+    "revision": "9d1ef2ffb79bef396f7b",
+    "url": "/static/js/2.9d1ef2ff.chunk.js"
+  },
+  {
+    "revision": "bfe609acb6eecbcb50ce",
+    "url": "/static/css/main.34ada41b.chunk.css"
+  },
+  {
+    "revision": "ee97e22d3f9a1f7178cb00ab1606ca64",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -3,13 +3,11 @@ var socket = require('socket.io');
 var path=require('path');
 var app = express();
 
-if(process.env.NODE_ENV === 'production')
-{
-	app.use(express.static('client/build'));
-	app.get('*', (req,res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+app.use(express.static(path.join(__dirname, 'Client' , 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html'));
+});
 
 
 server = app.listen(process.env.PORT || 5000, function(){
